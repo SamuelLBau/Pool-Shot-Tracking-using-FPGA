ec91e085045c9dd06d56a57ffec5c5983e184239
SamuelLBau <samuellbauza@gmail.com>
2016-05-03 14:49:41 -0700

    More set up for full program
    Set up main function to be ready to loop using identification function.
    
    Possibly found cause of crash, do not re-write to same variable too
    often. Use new variables.

--- ballDetection.py
+++ ballDetection.py
@@ -45,13 +45,13 @@ def drawBlobs(img):
 	im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	return im_with_keypoints
 
-def getHSVMaskExcludeTable(H,THRESHOLD=15000):
-	histogram = cv2.calcHist([H], [0], None, [180], [0, 180])
+def getHSVMaskExcludeTable(hsv,THRESHOLD=7000):
+	histogram = cv2.calcHist([hsv], [0], None, [180], [0, 180])
 
-	H[histogram[H][:,:,0] > THRESHOLD] = 0
+	hsv[histogram[hsv][:,:,0] > THRESHOLD] = 0
 	#print(hsv[:,:,0].shape)
-	H = cv2.threshold(H,1,255,cv2.THRESH_BINARY)[1]
-	return H
+	hsv = cv2.threshold(hsv,1,255,cv2.THRESH_BINARY)[1]
+	return hsv
 
 def getHSVMaskIncludeBalls(hsv):
 	#These are currently hard-coded, hopefully we can do something else with it later
@@ -101,34 +101,59 @@ def getHSVMaskIncludeBalls(hsv):
 
 	return outMask
 
-def getMask(H):
+def getMask(hsv,threshold):
     #Accepts just HUE as input
-    mask = getHSVMaskExcludeTable(H)
+    mask = getHSVMaskExcludeTable(hsv[:,:,0],THRESHOLD=threshold)
     mask = cv2.erode(mask, None, iterations=4)
-    mask = cv2.dilate(mask, None, iterations=9)
+    mask = cv2.dilate(mask, None, iterations=4)
     
     return mask
+def erodeAndDilate(mask,erodeIT=0,dilateIT=0):
+    mask = cv2.erode(mask, None, iterations=erodeIT)
+    mask = cv2.dilate(mask, None, iterations=dilateIT)
+    return mask
     
-def detectBlobs(frame,mask):
+def detectBlobs(mask):
 
     blobs = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[-2]
     return blobs
 	#return mask
 
-def getCircles(blobs):
+def getCircles(blobs,minRadius=0,maxRadius = 10000):
     circles = []
+    moments = []
     if len(blobs) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		#c = max(cnts, key=cv2.contourArea)
-		for c in blobs:
-			circles.append(cv2.minEnclosingCircle(c))
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        #c = max(cnts, key=cv2.contourArea)
+        for c in blobs:
+            moment = cv2.moments(c)
+            circle = cv2.minEnclosingCircle(c)
+            radius = circle[1]
+            if radius > minRadius and radius < maxRadius:
+                circles.append(circle)
+                moments.append(moment)
                     
-    return circles
-
-def drawFrame(frame,circles):
+    return [circles,moments]
+def drawCirclesOnFrame(frame,circles,moments):
+    length = len(moments)
+    i=0
+    while i < length:
+        M = moments[i]
+        ((x,y),radius) = circles[i]
+        if M["m00"] > 0:
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            # only proceed if the radius meets a minimum size
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        
+        
+        i=i+1
+    return frame
+def drawFrame(frame,circles,minRadius=0,maxRadius=10000):
     if(circles != None):
         for c in circles:
             ((x, y), radius) = cv2.minEnclosingCircle(c)
@@ -136,7 +161,7 @@ def drawFrame(frame,circles):
             if M["m00"] > 0:
                 center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
                 # only proceed if the radius meets a minimum size
-                if radius > 20 and radius < 50:
+                if radius > minRadius and radius < maxRadius:
                     # draw the circle and centroid on the frame,
                     # then update the list of tracked points
                     cv2.circle(frame, (int(x), int(y)), int(radius),
@@ -147,7 +172,7 @@ def drawFrame(frame,circles):
     return frame
 
     
-def getTable(hsv,frame=None,writer=None):        
+def getTable(hsv):        
     #Takes in a gaussian blurred image in HSV domain
     #This method still assumes the table takes up most of the image
     #It outputs the cropped / rotated image in hsv space
@@ -165,7 +190,6 @@ def getTable(hsv,frame=None,writer=None):
     thing = histogram[hsv[:,:,0]][:,:,0]
     mask[thing < THRESHOLD] = 0
     mask = cv2.threshold(mask,40,255,cv2.THRESH_BINARY)[1]
-    
     blobs = cv2.findContours(mask.copy(),contourMode,contourMethod)[1]
     
     maxContourArea = 0
@@ -179,80 +203,13 @@ def getTable(hsv,frame=None,writer=None):
             maxContourIndex = i
         i=i+1
     
-    hull = cv2.convexHull(blobs[maxContourIndex],returnPoints = True)
-    tableMask = cv2.bitwise_and(hsv[:,:,0],cv2.drawContours(np.zeros((width,length),np.uint8),[hull],0,(255),-1))
-    
-    #Threshold image, should help isolate table
-    THRESHOLD = width*length*.3
-    histogram = cv2.calcHist([mask], [0], None, [180], [0, 180])
-    
-    #tableMask[histogram[tableMask][:,:,0] < THRESHOLD] = 0
-    
-    minLineLength = 500
-    maxLineGap = 1
-    canny = cv2.Canny(mask,100,100,apertureSize = 3)
-    
-    showImage = imutils.resize(tableMask, width=850)
-    cv2.imshow('frame',showImage)
-    
-    lines = cv2.HoughLines(canny,2,np.pi/180*1,200)
-    if(lines != None):
-        cont = 0
-        print(len(lines))
-        for line in lines:
-            for rho,theta in line:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a*rho
-                y0 = b*rho
-                x1 = int(x0 + 1000*(-b))
-                y1 = int(y0 + 1000*(a))
-                x2 = int(x0 - 1000*(-b))
-                y2 = int(y0 - 1000*(a))
-                #cv2.line(mask,(x1,y1),(x2,y2),(255,255,0),2)
-        #for [[x1,y1,x2,y2]] in lines:
-         #   cv2.line(mask,(x1,y1),(x2,y2),(255,255,0),2)
-          #  cont=cont+1
-        #print(cont)
-    
-    
-    showImage = imutils.resize(mask, width=850)
-    #cv2.imshow('frame',showImage)
-    
     rect = cv2.minAreaRect(blobs[maxContourIndex])
     box = np.int0(cv2.boxPoints(rect))
     
-    #rotMatrix = getTable2DRotation(box,rect,width,length)
-    rotMatrix = getAffineRotation(box,rect,width,length)
-    
-    #This crops the image appropriately
-    #AFTER THIS POINT, mask is re-used to save space
-    #All references to mask could be replaced with OUTFRAME
-    outFrame = cv2.drawContours(np.zeros((width,length),np.uint8),[box],0,(255),-1)
-    #THIS IS JUST SOME DEBUG CODE
-    croppedFrame = frame
-    if(frame !=None):
-        showImage = frame
-        showImage[:,:,0] =cv2.bitwise_and(frame[:,:,0],outFrame)
-        showImage[:,:,1] =cv2.bitwise_and(frame[:,:,1],outFrame)
-        showImage[:,:,2] =cv2.bitwise_and(frame[:,:,2],outFrame)
-    
-        showImage = cv2.warpAffine(showImage,rotMatrix,(length,width))
-        croppedFrame = showImage
-        showImage = imutils.resize(showImage, width=850)
-        #cv2.imshow('frame',showImage)
-        if(writer != None):
-            writer.write(showImage)
-    
-    outFrame =cv2.bitwise_and(hsv[:,:,0],outFrame)
-    
-    outFrame = cv2.warpAffine(outFrame,rotMatrix,(length,width))
+    return box
     
 
-    #This simply displays the image
-    return [outFrame,croppedFrame]        #TEMPORARY, normally return table (holes) info
-
-def getAffineRotation(box,rect,width,length):
+def getAffineRotation(box,width,length):
     #TODO: This seems to mirror image for some reason, look into that
     yVarMax = 0
     xVarMax = 0
@@ -273,3 +230,33 @@ def getAffineRotation(box,rect,width,length):
     originalCoords = np.float32([TBOX[0],TBOX[1],TBOX[2]])
     newCoords = np.float32([[0,0],[0,width],[length,width]])
     return cv2.getAffineTransform(originalCoords,newCoords)
+
+def rotateFrame(box,frame):
+    #should work on both frame and HSV
+    #rotMatrix = getTable2DRotation(box,rect,width,length)
+    width,length,layers = frame.shape
+    rotMatrix = getAffineRotation(box,width,length)
+    
+    #This crops the image appropriately
+    #AFTER THIS POINT, mask is re-used to save space
+    #All references to mask could be replaced with OUTFRAME
+    mask = cv2.drawContours(np.zeros((width,length),np.uint8),[box],0,(255),-1)
+    outFrame = frame
+    #THIS IS JUST SOME DEBUG CODE   if(frame !=None):
+    outFrame[:,:,0] =cv2.bitwise_and(frame[:,:,0],mask)
+    outFrame[:,:,1] =cv2.bitwise_and(frame[:,:,1],mask)
+    outFrame[:,:,2] =cv2.bitwise_and(frame[:,:,2],mask)
+    
+    outFrame[:,:,0] = cv2.warpAffine(outFrame[:,:,0],rotMatrix,(length,width))
+    outFrame[:,:,1] = cv2.warpAffine(outFrame[:,:,1],rotMatrix,(length,width))
+    outFrame[:,:,2] = cv2.warpAffine(outFrame[:,:,2],rotMatrix,(length,width))
+    
+
+    #This simply displays the image
+    return outFrame        #TEMPORARY, normally return table (holes) info
+    
+def averageBox(box,newbox,count):
+    for i in range(0,3):
+        box[i][0] = box[i][0]*(count)/(count+1) + newbox[i][0]/(count+1)
+        box[i][1] = box[i][1]*(count)/(count+1) + newbox[i][1]/(count+1)
+    return box
--- main.py
+++ main.py
@@ -21,6 +21,11 @@ RESULT_VIDEO_NAME = VIDEO_IN_NAME+"_result.mp4"
 VIDEO_IN_NAME = VIDEO_IN_NAME+ VIDEO_EXTENSION
 fourcc = cv2.VideoWriter_fourcc("X","V","I","D")
 
+
+
+
+NUM_TABLE_FRAMES_AVERAGED = 10
+
 cap = cv2.VideoCapture(VIDEO_IN_NAME)
 
 
@@ -28,18 +33,21 @@ ret, frame = cap.read()
 frameHeight,frameWidth,depth = frame.shape
 #(*'H264')
 
+frame = cv2.GaussianBlur(frame,(3,3),0)
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   
+box = getTable(hsv)
 
 
 
 
-
-#maskWriter = cv2.VideoWriter(MASK_VIDEO_NAME ,fourcc,30,(frameWidth,frameHeight),0)
-tableWriter = cv2.VideoWriter(TABLE_VIDEO_NAME,fourcc,30,(frameWidth,frameHeight)) 
+maskWriter = cv2.VideoWriter(MASK_VIDEO_NAME ,fourcc,30,(frameWidth,frameHeight),0)
+#tableWriter = cv2.VideoWriter(TABLE_VIDEO_NAME,fourcc,30,(frameWidth,frameHeight)) 
 #circlesWriter = cv2.VideoWriter(CIRCLE_VIDEO_NAME,fourcc,30,(frameWidth,frameHeight))
 #resultWriter = cv2.VideoWriter(RESULT_VIDEO_NAME,fourcc,30,(frameWidth,frameHeight))
 #fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 #fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
 
+frameCount = 1
 while(cap.isOpened()):
     ret, frame = cap.read()
     #bgsub = subtractBackground(fgbg,frame)
@@ -51,30 +59,44 @@ while(cap.isOpened()):
     frame = cv2.GaussianBlur(frame,(3,3),0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
+    #This only searches for table for the first few frames
+    #It averages the found corners, hopefully to limit
+    if(frameCount < NUM_TABLE_FRAMES_AVERAGED): 
+        newBox = getTable(hsv)
+        box = averageBox(box,newBox,frameCount)
+        frameCount = frameCount +1
     
-    [H,tableFrame] = getTable(hsv,frame)
-    #mask = getMask(H)
-    #blobs = detectBlobs(frame,mask)
-    #circles = getCircles(blobs)
+    #This sets up frame for ball searching
+    hsv = rotateFrame(box,hsv)    #This should return the hsv with the table isolated
+    outFrame = rotateFrame(box,frame)
+    mask = getMask(hsv,threshold=8000)#This does some light dilation, prevents some crash for some reason
     
-
-    #outFrame = drawFrame(frame,blobs)
+    #This should be the iterative logic that tries to isolate balls
+    #TODO: ADD ITERATIVE LOGIC HERE
+    Nmask = erodeAndDilate(mask,erodeIT=4,dilateIT=4)
+    #mask = None
+    mask = Nmask.copy()
+    #Nmask = None
+    blobs = detectBlobs(mask)
+    [circles,moments] = getCircles(blobs,minRadius=30,maxRadius=130)
     
-    #maskWriter.write(mask)
+    outFrame = drawCirclesOnFrame(outFrame,circles,moments)
+    #outFrame = drawFrame(outFrame,blobs,minRadius=30,maxRadius=130)
+    
+    maskWriter.write(mask)
     #print(frame.shape)
     #print(tableFrame.shape)
-    tableWriter.write(tableFrame)
+    #tableWriter.write(tableFrame)
     #resultWriter.write(outFrame)
     #circlesWriter.write(outFrame)
-    
-    outFrame = imutils.resize(tableFrame, width=850)
-    #cv2.imshow('frame', outFrame)
+    outFrame = imutils.resize(outFrame, width=850)
+    cv2.imshow('frame', outFrame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-#maskWriter.release()
-tableWriter.release()
+maskWriter.release()
+#tableWriter.release()
 #circlesWriter.release()
 #resultWriter.release()
 cap.release()
